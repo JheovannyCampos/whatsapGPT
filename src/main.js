32748d1805dcb66d657bbe6c1cdc10d8d5a74d06
JheovannyCampos <jheovanny06@hotmail.com>
2023-03-28 00:09:45 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,40 @@
 //main.js
 const venom = require("venom-bot");
+const fs = require("fs");
 const { response } = require("./chatgpt");
 
 venom
-  .create({
-    session: "session-name", //name of session
-    multidevice: true, // for version not multidevice use false.(default: true)
+  .create(
+    "sessionName",
+    (base64Qr, asciiQR, attempts, urlCode) => {
+      console.log(asciiQR); // Optional to log the QR in the terminal
+      var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+        response = {};
+
+      if (matches.length !== 3) {
+        return new Error("Invalid input string");
+      }
+      response.type = matches[1];
+      response.data = new Buffer.from(matches[2], "base64");
+
+      var imageBuffer = response;
+      require("fs").writeFile(
+        "out.png",
+        imageBuffer["data"],
+        "binary",
+        function (err) {
+          if (err != null) {
+            console.log(err);
+          }
+        }
+      );
+    },
+    undefined,
+    { logQR: false }
+  )
+  .then((client) => {
+    start(client);
   })
-  .then((client) => start(client))
   .catch((erro) => {
     console.log(erro);
   });
